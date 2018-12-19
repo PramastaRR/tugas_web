a3bc5acd26857ec21be19b6a90fa45eab34cee44
Pramasta Ramadha Rahardjo <pramastarr@gmail.com>
2018-12-19 18:58:59 +0700

    Mengganti nama database di connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "puskesmas_sawangan";
+$dbname = "puskesmas";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
